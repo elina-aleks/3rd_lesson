7b9bffed1a81340efbf58e6166b09fbc23e32495
Elina <elina.aleksejeva@yahoo.com>
2021-09-16 14:21:30 +0300

    update

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,12 +1,52 @@
 ﻿using System;
 
 namespace ConsoleApp1
+/*
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hewwo World!");
+            Person person = new Person("Ellie");
+
+            Person person2 = person;
+
+            // person.Name = "Ellie";
+            person.Age = 100;
+            person.IsMale = false;
+            person.Money = 154.58;
+            person.Initial = 'A';
+
+            person2.Age = 26;
+
+            string greeting = person.GetGreeting();
+            Console.WriteLine(greeting);
+        }
+    }
+}
+*/
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Phone phone = new Phone("Telefons");
+
+            phone.Brand = "Blackberry";
+            phone.Type = "Dual Sim";
+            phone.Size = 14.7;
+            phone.Call = true;
+            phone.Text = true;
+
+            string greeting = phone.GetGreeting();
+            Console.WriteLine(greeting);
+
+            string call = phone.Call();
+            Console.WriteLine(call);
+
+            string text = phone.Text();
+            Console.WriteLine(text);
         }
     }
 }
---
+++ ConsoleApp1/phone.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+
+namespace ConsoleApp1
+{
+    public class Phone
+    {
+        public Phone(string brand)
+        {
+            Brand = brand;
+        }
+        public string Brand { get; set; }
+        public string Type { get; set; }
+        public double Size { get; set; }
+
+        /*
+                public bool Call { get; set; }
+                public bool Text { get; set; }
+        */
+
+        public string Call()
+        {
+            string call = "Telefons var zvanīt.";
+            return call;
+        }
+
+        public string Text()
+        {
+            string text = "Telefons lietot SMS.";
+            return text;
+        }
+
+        public string GetGreeting()
+        {
+            string greeting = "Telefona marka ir" + " " + Brand + " " + "Un teleforna modelis ir" + " " + Type + " " + "telefona izmers ir" + " " + Size + " " + "cm.";
+            return greeting;
+
+        }
+
+    }
+}
